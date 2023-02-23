225c66682745654768ac2bd9ab0e1d7af61b6b43
Hugo Souza <hugo.html.com@gmail.com>
2023-02-22 22:06:30 -0300

    refactor: Refatorar cardSearch, CardInfo, api

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type Props = {
+        avatar_url:String,
+        bio: String,
+        followers: number,
+        following: number,
+        location: string,
+        name: String,
+        id?:number,
+}
